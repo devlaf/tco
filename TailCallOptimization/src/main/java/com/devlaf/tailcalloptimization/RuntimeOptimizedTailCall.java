2a51bf91527d73466f32dab0a9544875c186cef1
devlaf <devin@devlaf.io>
2020-03-04 01:41:53 -0600

    Add javadocs

--- TailCallOptimization/src/main/java/com/devlaf/tailcalloptimization/RuntimeOptimizedTailCall.java
+++ TailCallOptimization/src/main/java/com/devlaf/tailcalloptimization/RuntimeOptimizedTailCall.java
@@ -6,12 +6,23 @@ import com.devlaf.tailcalloptimization.trampoline.Trampoline;
 
 import java.util.function.Function;
 
+/**
+ * A framework for creating recursive functions that will provide a poor-man's tail-call optimization at runtime.
+ * @param <T> An object type containing any data that's traditionally passed on as part of recursion.
+ * @param <Q> The expected return type
+ */
 public class RuntimeOptimizedTailCall<T,Q> {
     private final Trampoline<T> trampoline;
     private final Function<T, Boolean> endCondition;
     private final Function<T,Q> resultFromEndCondition;
     private final Function<T,T> continuation;
 
+    /**
+     * @param seed The initial T to seed the recursive func.
+     * @param endCondition A function to determine, given T, whether this T represents the base case.
+     * @param resultFromEndCondition A func to extract the value from T at the base case.
+     * @param continuation A function to generate the new T to pass on with the recursive operation.
+     */
     public RuntimeOptimizedTailCall(T seed,
                                     Function<T, Boolean> endCondition,
                                     Function<T,Q> resultFromEndCondition,
